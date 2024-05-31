b2cd36d5b339a62cf550c9529db00f612e6b7a09
JulianusIV <ivjulianus@gmail.com>
2024-06-01 01:41:15 +0200

    some touchups on burst fire logic

--- XDefiantWeaponStats/Program.cs
+++ XDefiantWeaponStats/Program.cs
@@ -7,10 +7,5 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped(sp =>
-    new HttpClient
-    {
-        BaseAddress = new Uri("https://gist.githubusercontent.com/JulianusIV/048279b4720afdb062366c276026d0a3/raw/080697ea0ccf596bce1fb09062c2470a864f6c60/test.json")
-    });
 
 await builder.Build().RunAsync();
